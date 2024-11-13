4b28ba2be55286248abc802f68361713be66a87b
João Vieira <joaolol50@gmail.com>
2024-11-13 20:35:40 -0300

    Novo

---
+++ index.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+
+    <h1>Logi Perifericos</h1>
+    <h2> Para os melhores Gamers, os Melhores perifericos sao necessarios!. De o melhor de se com o melhor dos equipamentos, Logitech junto da sua gameplay!</h2>
+    <h2> Descubra nossos novos produtos e os mais queridinhos dos Streamers. Nao perca essa chance, aproveite ja com os melhores perifericos disponiveis da atualidade!</h2>
+    <br/><br/><br/><br/><br/>
+    <div class="loja">
+        <h1>Menu de operações</h1>
+        <button id="cadastroBtn">Cadastro </button><br/><br/>
+        <button id="vendaBtn">Venda</button>
+    </div>
+    <script type="text/javascript" src="js/menu.js"></script>
+</body>
+</html>
